f86729fc489b59ed688b9bc1bff5c4838110e1ba
Jitendra Kumar <jitendra.kumar@neosofttech.com>
2017-11-06 16:11:38 +0530

    update server.js

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ app.set('port', config.PORT);
 
 app.use(bodyParser.json());
 
+if (app.get('env') === 'production') {
+    app.use((req, res, next) => {
+        var protocol = req.get('x-forwarded-proto');
+        protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
+    });
+}
+
 require('./utils/db');
 
 app.use('/api', require('./modules/user/router'));
 
-app.listen(app.get('port'), function(err){
-    if(err){
+app.listen(app.get('port'), (err) => {
+    if (err) {
         return console.log('Error', err);
     }
     console.log(`Server is listening on ${app.get('port')}`);
